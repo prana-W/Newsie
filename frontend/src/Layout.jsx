6b9dc3c23d217047e3bacb6078de5aa871f0dda0
Ashutosh <ashutoshjee2024@gmail.com>
2026-03-24 22:11:48 +0530

    fixed the responsive issue in displaying the news

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -5,7 +5,7 @@ import {Toaster} from '@/components/ui/sonner';
 function Layout() {
     return (
         <>
-            <div className="relative w-full h-full md:w-[400px] md:h-[860px] md:max-h-[95vh] md:rounded-[2.5rem] md:overflow-hidden md:shadow-[0_32px_80px_rgba(0,0,0,0.8)] md:border md:border-white/5">
+            <div className="relative w-full h-full sm:w-full sm:max-w-md sm:h-screen sm:mx-auto sm:my-0 md:w-[400px] md:h-[860px] md:max-h-[95vh] md:rounded-[2.5rem] md:overflow-hidden md:shadow-[0_32px_80px_rgba(0,0,0,0.8)] md:border md:border-white/5 overflow-hidden">
         <Outlet />
       </div>
         </>
